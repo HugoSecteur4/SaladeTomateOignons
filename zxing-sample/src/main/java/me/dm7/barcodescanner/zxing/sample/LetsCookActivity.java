032a80e937784cdeb464ad62a035f731faf77184
TERRIER <bastien.terrier7@gmail.com>
2018-11-19 21:58:51 +0100

    changement des etapes, ajout d'images pour le riz au lait

--- zxing-sample/src/main/java/me/dm7/barcodescanner/zxing/sample/LetsCookActivity.java
+++ zxing-sample/src/main/java/me/dm7/barcodescanner/zxing/sample/LetsCookActivity.java
@@ -23,13 +23,13 @@ public class LetsCookActivity extends AppCompatActivity {
         setContentView(R.layout.activity_lets_cook);
 
         ArrayList<Etape> etapes = new ArrayList<Etape>();
-        Etape etape1 = new Etape("Etape 1", "Faire bouillir l’eau dans une casserole. Mettre le riz et le laisser bouillir 2 à 3 minutes.", 0);
-        Etape etape2 = new Etape("Etape 2", "Égoutter le riz. ", 0);
-        Etape etape3 = new Etape("Etape 3", "Faire chauffer le lait avec le zeste râpé du citron, et les sucres. ", 0);
-        Etape etape4 = new Etape("Etape 4", "Verser le riz.", 0);
-        Etape etape5 = new Etape("Etape 5", "Faire bouillir, puis laisser cuire à feu doux sans remuer. ", 0);
-        Etape etape6 = new Etape("Etape 6", "Le riz est cuit lorsqu’il a absorbé tout le lait. ", 0);
-        Etape etape7 = new Etape("Etape 7", "Servir froid ou tiède.", 0);
+        Etape etape1 = new Etape("Etape 1", "Faire bouillir l’eau dans une casserole. Mettre le riz et le laisser bouillir 2 à 3 minutes.", R.drawable.etape1r);
+        Etape etape2 = new Etape("Etape 2", "Égoutter le riz. ", R.drawable.etape2r);
+        Etape etape3 = new Etape("Etape 3", "Faire chauffer le lait avec le zeste râpé du citron, et les sucres. ", R.drawable.etape3r);
+        Etape etape4 = new Etape("Etape 4", "Verser le riz.", R.drawable.etape4r);
+        Etape etape5 = new Etape("Etape 5", "Faire bouillir, puis laisser cuire à feu doux sans remuer. ", R.drawable.etape5r);
+        Etape etape6 = new Etape("Etape 6", "Le riz est cuit lorsqu’il a absorbé tout le lait. ", R.drawable.etape6r);
+        Etape etape7 = new Etape("Etape 7", "Servir froid ou tiède.", R.drawable.etape7r);
         etapes.add(etape1);
         etapes.add(etape2);
         etapes.add(etape3);
@@ -41,11 +41,11 @@ public class LetsCookActivity extends AppCompatActivity {
 
 
         ArrayList<Etape> recetteEau = new ArrayList<Etape>();
-        Etape etape1_eau = new Etape("Etape 1", "Prenez le verre 2 dans la main gauche.", R.drawable.etape1);
-        Etape etape2_eau = new Etape("Etape 2", "Prenez la bouteille d'eau dans la main droite.", R.drawable.etape2);
-        Etape etape3_eau = new Etape("Etape 3", "Remplir le verre 2 jusqu'au trait.", R.drawable.etape3);
-        Etape etape4_eau = new Etape("Etape 4", "Répétez les opérations suivantes avec le verre 3.", R.drawable.etape4);
-        Etape etape5_eau = new Etape("Etape 5", "Remplir le verre 1 à l'aide du verre 2 puis du verre 3. Servir bien frais !", R.drawable.etape5);
+        Etape etape1_eau = new Etape("Etape 1", "Prenez le verre avec le numéro deux dans votre main gauche.", R.drawable.etape1);
+        Etape etape2_eau = new Etape("Etape 2", "Prenez la bouteille d'eau dans votre main droite.", R.drawable.etape2);
+        Etape etape3_eau = new Etape("Etape 3", "Remplir le verre numéro deux jusqu'au trait bleu.", R.drawable.etape3);
+        Etape etape4_eau = new Etape("Etape 4", "Répétez l'opération précédente avec le verre numéro trois.", R.drawable.etape4);
+        Etape etape5_eau = new Etape("Etape 5", "Remplir le verre numéro un avec les contenus des verres numéros deux et trois. Ce cocktail est à boire très frais !", R.drawable.etape5);
         recetteEau.add(etape1_eau);
         recetteEau.add(etape2_eau);
         recetteEau.add(etape3_eau);
